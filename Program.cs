b93c6389667b7b763eaf4e8a177a7f86afb44359
RayanEreteo <rayabf5@gmail.com>
2026-01-21 20:13:33 +0100

    Added comments

--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@ class DreamJournalCLI
 {
     static void Main(string[] args)
     {
+        // If no arguments, show the guide explaining all the commands.
         if (args.Length == 0)
         {
             Guide.ShowGuide();
             return;
         }
 
+        // Get the main command from the user.
+        string command = args[0];
+
+        CommandHandling.CommandInit(command);
     }
 }
