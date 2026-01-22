56364662be68fab439d8f14b635cbeb3db40c380
RayanEreteo <rayabf5@gmail.com>
2026-01-22 13:53:47 +0100

    Check for flag on the change case witht he help of Helpers class

--- CommandHandling.cs
+++ CommandHandling.cs
@@ -1,12 +1,17 @@
 class CommandHandling
 {
-    public static void CommandInit(string command)
+    public static void CommandInit(string command, string[] args)
     {
         switch (command)
         {
             case "donate":
                 // Open browser to ko-fi link for donation.
                 CommandActions.Donate();
+                return;
+            case "change":
+                string flag = Helpers.FlagRequired(args);
+
+                CommandActions.Change(flag);
                 return;
             default:
                 // Exit with a message if invalid command.
