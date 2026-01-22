dab1fa77c4543d6f414cc7859d8e05fd2ead64b7
RayanEreteo <rayabf5@gmail.com>
2026-01-22 10:56:56 +0100

    Added "donate" command to CommandHandling.cs
    CommandInit will now switch on command and will stop if invalid command is found.

--- CommandHandling.cs
+++ CommandHandling.cs
@@ -1,8 +1,22 @@
+using System.Diagnostics;
+
 class CommandHandling()
 {
     public static void CommandInit(string command)
     {
-        Console.WriteLine(command);
+        switch (command)
+        {
+            case "donate":
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "https://ko-fi.com/rayanereteo",
+                    UseShellExecute = true,
+                });
+                return;
+            default:
+                Console.WriteLine("Invalid Command");
+                return;
+        }
     }
 
     private static void CommandActions()
