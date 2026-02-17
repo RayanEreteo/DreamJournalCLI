4eb1b8c2346bd18f61ba6fde5edd97b3d57357a7
RayanEreteo <rayabf5@gmail.com>
2026-02-17 16:56:33 +0100

    Initial Commit
    Added guide if user don't pass arguments

---
+++ guide.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void ShowGuide()
+{
+    printf("Dream Journal CLI\n");
+    printf("=================\n\n");
+
+    printf("USAGE:\n");
+    printf("  command [options] [arguments]\n\n");
+
+    printf("COMMANDS:\n");
+
+    printf("  add <title> <content> <lucid>\n");
+    printf("    Add a new dream entry to the journal.\n");
+    printf("    - title   : String (use quotes if it contains spaces)\n");
+    printf("    - content : String (use quotes if it contains spaces)\n");
+    printf("    - lucid   : Boolean (true or false)\n");
+    printf("    Example:\n");
+    printf("      add \"Flying Dream\" \"I was flying over a city\" true\n\n");
+
+    printf("  remove <title>\n");
+    printf("    Remove a dream entry by its title.\n");
+    printf("    Example:\n");
+    printf("      remove \"Flying Dream\"\n\n");
+
+    printf("  journal\n");
+    printf("    Display all journal entries.\n\n");
+
+    printf("  journal -limit <number>\n");
+    printf("    Display the last <number> of entries.\n");
+    printf("    Example:\n");
+    printf("      journal -limit 5\n\n");
+
+    printf("  change -path <path>\n");
+    printf("    Set the folder where the journal is saved.\n");
+    printf("    Example:\n");
+    printf("      change -path C:\\\\DreamJournal\n\n");
+
+    printf("  change -startup-launch <Y/N>\n");
+    printf("    Enable or disable launch on Windows startup.\n");
+    printf("    Example:\n");
+    printf("      change -startup-launch Y\n\n");
+
+    printf("  donate\n");
+    printf("    Open the Ko-fi donation page in your browser.\n");
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "guide.h"
+
+int main(int argc, char *argv)
+{
+    if(argc == 1)
+    {
+        ShowGuide();
+    }
+}
