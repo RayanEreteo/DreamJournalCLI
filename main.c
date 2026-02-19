9f2108e4f93d2358aaab58f87445d49cc7af7bfc
RayanEreteo <rayabf5@gmail.com>
2026-02-19 10:42:34 +0100

    run commandInit on main if args

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+
+#include "./headers/command.h"
 #include "./headers/guide.h"
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
-    if(argc == 1)
+    if (argc == 1)
     {
         ShowGuide();
+        return 0;
     }
+    
+    char *command = argv[1];
+
+    CommandInit(command, argv);
 }
